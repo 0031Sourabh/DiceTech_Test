94b0c9794c7a96111aac632d54bd19d0001ac8b4
0031Sourabh <sshinde0031@gmail.com>
2022-07-17 22:31:15 +0530

    first

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 1, 'Mumbai', 'akshay@gmail.com', 1001, 'Akshay Duduskar', '2017-04-02', '+9112345678' );
+
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 2, 'Thane', 'rav@gmail.com', 1002, 'Ravleen Pulaha', '2021-10-08', '+9174002711' );
+
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 3, 'Nagpur', 'meena@gmail.com', 1003, 'Maninder Pulaha', '1970-04-30', '+91909644098' );
+
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 4, 'Pune', 'minal@gmail.com', 1004, 'Minal Patil', '2021-08-10', '+91098765364' );
+
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 5, 'Solapur', 'sou@gmail.com', 1005, 'Sourabh Shinde', '2021-08-10', '+91234569876' );
+
+insert into EMPLOYEES ( SR_NO, ADDRESS, EMAIL_ID, EMP_ID, EMP_NAME, JOIN_DATE, MOBILE_NO)
+values ( 6, 'Mumbai', 'sumit@gmail.com', 1006, 'Sumit', '2019-08-05', '+919920834856' );
